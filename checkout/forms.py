d96f90c6fd0812a5a89865c5170031666e91ada6
d-lynch95 <darraghlynch1995@gmail.com>
2023-10-16 12:22:52 +0000

    added stripe elements

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,7 +3,7 @@ from .models import Order
 
 class OrderForm(forms.ModelForm):
     class Meta:
-        model = order
+        model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country',
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from .forms import OrderForm
 
 # Create your views here.
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51O1pBWGUTlChP7ZhLEIsUQbdYTC1mJubkK6DqFqCLsqj04bTaUNKpzyXlQhC90LolSIo19dVrOZNzXMG20Ly0gGw00CpiVfVnq',
+        'client_secret': 'test client secret',
+    }
+
+    return render(request, template, context)
