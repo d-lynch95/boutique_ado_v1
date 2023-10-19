4dd6ddd19f5ec4e7887fdc7e80c9e634db4998ea
d-lynch95 <darraghlynch1995@gmail.com>
2023-10-19 10:15:59 +0000

    added order summary to checkout success page

--- checkout/models.py
+++ checkout/models.py
@@ -6,7 +6,6 @@ from django.conf import settings
 
 from products.models import Product
 
-# Create your models here.
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
@@ -24,18 +23,18 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
-    def _generate_order_name(self):
+    def _generate_order_number(self):
         """
         Generate a random, unique order number using UUID
         """
-        return uuid.uuid(4).hex.upper()
+        return uuid.uuid4().hex.upper()
 
     def update_total(self):
         """
         Update grand total each time a line item is added, accounting
         for delivery costs
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -59,7 +58,7 @@ class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL 
-    quantity = models.IntegerField(null=False, blank=False, default = 0)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
